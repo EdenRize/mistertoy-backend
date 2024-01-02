efcf3ec5d352e2b5346cae1772428bbd2a268892
EdenRize <edenrize@gmail.com>
2024-01-02 16:59:46 +0200

    Init services

--- server.js
+++ server.js
@@ -1,49 +1,71 @@
+import http from 'http'
+import path from 'path'
+import cors from 'cors'
 import express from 'express'
 import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import path, { dirname } from 'path'
-import { fileURLToPath } from 'url'
+// import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const app = express()
+const server = http.createServer(app)
+
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = dirname(__filename)
 
 import { logger } from './services/logger.service.js'
 logger.info('server.js loaded...')
 
-const app = express()
 
 // Express App Config
 app.use(cookieParser())
 app.use(express.json())
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
 if (process.env.NODE_ENV === 'production') {
-    // Express serve static files on production environment
-    app.use(express.static(path.resolve(__dirname, 'public')))
-    console.log('__dirname: ', __dirname)
+    app.use(express.static(path.resolve('public')))
 } else {
-    // Configuring CORS
     const corsOptions = {
-        // Make sure origin contains the url your frontend is running on
-        origin: ['http://127.0.0.1:5173', 'http://localhost:5173', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+        origin: ['http://127.0.0.1:3000',
+            'http://localhost:3000',
+            'http://127.0.0.1:5173',
+            'http://localhost:5173'
+        ],
         credentials: true
     }
     app.use(cors(corsOptions))
 }
 
+// if (process.env.NODE_ENV === 'production') {
+//     // Express serve static files on production environment
+//     app.use(express.static(path.resolve(__dirname, 'public')))
+//     console.log('__dirname: ', __dirname)
+// } else {
+//     // Configuring CORS
+//     const corsOptions = {
+//         // Make sure origin contains the url your frontend is running on
+//         origin: ['http://127.0.0.1:5173', 'http://localhost:5173', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+//         credentials: true
+//     }
+//     app.use(cors(corsOptions))
+// }
+
 // routes
 
 import { authRoutes } from './api/auth/auth.routes.js'
-app.use('/api/auth', authRoutes)
-
 import { userRoutes } from './api/user/user.routes.js'
-app.use('/api/user', userRoutes)
-
 import { toyRoutes } from './api/toy/toy.routes.js'
-app.use('/api/toy', toyRoutes)
-
 import { reviewRoutes } from './api/review/review.routes.js'
+
+// routes
+import { setupAsyncLocalStorage } from './middlewares/setupAls.middleware.js'
+app.all('*', setupAsyncLocalStorage)
+
+
+app.use('/api/auth', authRoutes)
+app.use('/api/user', userRoutes)
+app.use('/api/toy', toyRoutes)
 app.use('/api/review', reviewRoutes)
+setupSocketAPI(server)
+
 
 // Make every unmatched server-side-route fall back to index.html
 // So when requesting http://localhost:3030/index.html/car/123 it will still respond with
